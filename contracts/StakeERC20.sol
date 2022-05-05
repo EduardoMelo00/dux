04c7513de5558d95937b888099344e032be77ad7
EduardoMeloDev <78520501+EduardoMeloDev@users.noreply.github.com>
2022-05-05 14:12:03 -0300

    Alterar função de getRewards e withdraw para utilizar apenas o transfer

--- contracts/StakeERC20.sol
+++ contracts/StakeERC20.sol
@@ -6,9 +6,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "../interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import "./Mana.sol";
-import "./Dux.sol";
+//import "./matic.sol";
+//import "./Dux.sol";
 
 contract StakeERC20 is ReentrancyGuard, Ownable, Pausable {
     using SafeMath for uint256;
@@ -16,8 +17,11 @@ contract StakeERC20 is ReentrancyGuard, Ownable, Pausable {
     AggregatorV3Interface internal TokenPriceFeed;
     uint256 public rewardFactor = 1; // 1 = 1 per cent
     uint256 public rewardInterval = 86400/24/60; // 86400 = 1 day
-
-
+    uint256 public duxPrice = 25 ; // DUX price 
+    
+    IERC20 public link; 
+    IERC20 public dux; 
+    
     struct StakedToken {
         uint256 amount;
         uint256 startTimestamp;
@@ -26,34 +30,30 @@ contract StakeERC20 is ReentrancyGuard, Ownable, Pausable {
     mapping(address => StakedToken) public stakedTokens; // owner => StakedToken
     mapping(address => uint256) public accruedReward;
 
-    Mana public mana;
-    Dux public dux;
-
     constructor(
-        Mana _mana,
-        Dux _dux
-    ) {
-        mana = _mana;
-        dux = _dux;
 
-    // price feed MATIC / USD rinkeby
+    ) {
+    // price feed ETHER / USD rinkeby
       TokenPriceFeed = AggregatorV3Interface(
-            0x7794ee502922e2b723432DDD852B3C30A911F021
+            0xd8bD0a1cB028a31AA859A21A3758685a95dE4623
         );
+         link = IERC20(0x01BE23585060835E02B77ef475b0Cc51aA1e0709);
+         dux   = IERC20(0x1f42e40BC1cA24609200cf6Eae2e9662FfE35869);   
     }
 
    function stake(uint256 _amount) public {
-        require(mana.balanceOf(msg.sender) >= _amount, "You don't own this amount of MANA.");
+        require(link.balanceOf(msg.sender) >= _amount, "You don't own this amount of matic.");
         if(stakedTokens[msg.sender].amount > 0){
             incrementAccruedReward(msg.sender);
         }
-        mana.transferFrom(msg.sender, address(this), _amount * 1e18);
+        link.transferFrom(msg.sender, address(this), _amount * 1e18);
         stakedTokens[msg.sender] = StakedToken(stakedTokens[msg.sender].amount + (_amount  * 1e18), block.timestamp);
    }
 
    function calculateReward(address _owner) public view returns (uint256){
         StakedToken memory staked = stakedTokens[_owner];
-        return (staked.amount * rewardFactor * (block.timestamp - staked.startTimestamp)) / 100 / rewardInterval;
+        uint256 rewardPriceUsd = ((staked.amount * (getLatestEthPrice() / 1e8))  * rewardFactor * (block.timestamp - staked.startTimestamp)) / 100 / rewardInterval;
+        return rewardPriceUsd / duxPrice / 100;                   
    }
 
     function incrementAccruedReward(address _owner) internal {
@@ -63,18 +63,19 @@ contract StakeERC20 is ReentrancyGuard, Ownable, Pausable {
         accruedReward[_owner] += reward;
    }
 
-   function withdraw() public {
+    function withdraw() private {
         StakedToken memory staked = stakedTokens[msg.sender];
         incrementAccruedReward(msg.sender);
-        mana.transferFrom(address(this), msg.sender, staked.amount);
+        link.transfer(msg.sender, staked.amount);
    }
 
     function getRewards() public {
         withdraw();
         uint256 rewards = accruedReward[msg.sender];
-        dux.transferFrom(address(this), msg.sender, rewards);
+        dux.transfer(msg.sender, rewards);
         stakedTokens[msg.sender].amount = 0;
         stakedTokens[msg.sender].startTimestamp = 0;
+        accruedReward[msg.sender] = 0;
     }
 
  function getLatestEthPrice() public view returns (uint256 latestTokenPrice) {
