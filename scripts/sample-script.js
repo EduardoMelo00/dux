e5c86396921560e0636fad26ed697d7d65a32afb
EduardoMeloDev <78520501+EduardoMeloDev@users.noreply.github.com>
2022-05-05 00:16:10 -0300

    first update

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello");
+  const Greeter = await hre.ethers.getContractFactory("StakeERC20");
+  const greeter = await Greeter.deploy();
 
   await greeter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("DUX deployed to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
